b66f8f904e1724847cb746fddd9e80f5ff292d91
N McKenzie <nina.mckenzie@gmail.com>
2025-12-17 14:43:20 +0000

    fix to list unit test

--- src/azure-blob/___tests___/list.test.js
+++ src/azure-blob/___tests___/list.test.js
@@ -1,21 +1,28 @@
 const request = require('supertest');
-const app = require('../index');
 
 describe('GET /files', () => {
+  beforeEach(() => {
+    // 1. Clear the cache so index.js re-initializes for every test
+    jest.resetModules();
+  });
+
   test('should return the list of files', async () => {
+    const app = require('../index'); // Require inside the test
     const response = await request(app).get('/files');
     
     expect(response.statusCode).toBe(200);
     expect(Array.isArray(response.body)).toBe(true);
   });
-  
+
   test('should return an empty array if the data file does not exist', async () => {
+    // 2. Mock fs before requiring the app so it sees the "false" value
     const fs = require('fs');
-    // Mocking specifically for this test
-    jest.spyOn(fs, 'existsSync').mockReturnValueOnce(false);
+    jest.spyOn(fs, 'existsSync').mockReturnValue(false);
     
+    const app = require('../index'); 
     const response = await request(app).get('/files');
+
     expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual([]);
+    expect(response.body).toEqual([]); // This should now be empty
   });
 });
